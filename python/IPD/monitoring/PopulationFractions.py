09ae56c9fb0e94426ad5f3b46ed8003a3ba2b12d
schoef <robert.schoefbeck@cern.ch>
2016-11-01 21:55:11 +0100

    monitoring, run.py

--- python/IPD/monitoring/PopulationFractions.py
+++ python/IPD/monitoring/PopulationFractions.py
@@ -7,12 +7,20 @@ from strategy.all import *
 
 class PopulationFractions( MonitorElementBase ):
 
-    def __init__ ( self ):
+    def __init__ ( self, filename ):
         self.population_data = {}
-        self.n = 0
+        self.n = 0  
+        self.filename = filename
     
     def initialize_arena( self, arena ):
        self.total_agents = len(arena.positions)
+
+    colors = ['k', 'b', 'g', 'r', 'c', 'm', 'y']
+    linestyles  = [ '-', '--', ':', '*', 'x']
+
+    @staticmethod
+    def style( i ):
+        return PopulationFractions.colors[i%len(PopulationFractions.colors)] + PopulationFractions.linestyles[i/len(PopulationFractions.colors)]
 
     @property
     def strategies( self ):
@@ -24,35 +32,33 @@ class PopulationFractions( MonitorElementBase ):
         arena_strategies = [ type(arena.agents[p]) for p in  arena.positions ]
         unique_arena_strategies = list(set(arena_strategies))
 
-        # Known strategies
+        # Count known strategies
         for s in self.population_data.keys():
             self.population_data[s].append( arena_strategies.count( s ) )
 
-        # New strategies
+        # Count strategies not yet known
         for s in unique_arena_strategies:
             if s in self.population_data.keys(): continue
             self.population_data[s] = [0 for i in range(self.n)] + [ arena_strategies.count( s ) ]
 
         self.n += 1
 
-
     def write( self ):
         import numpy as np
         import matplotlib.pyplot as plt
 
         data_x = xrange(self.n)
-        data_y_0 = [ p/float(self.total_agents) for p in self.population_data[self.strategies[0]] ]
-        data_y_1 = [ p/float(self.total_agents) for p in self.population_data[self.strategies[1]] ]
-        data_y_2 = [ p/float(self.total_agents) for p in self.population_data[self.strategies[2]] ]
-        plt.plot(\
-                 data_x, data_y_0, 'b-', 
-                 data_x, data_y_1, 'g-',
-                 data_x, data_y_2, 'r-'
-                )
+
+        for i_s, s in enumerate( self.strategies ):
+            data_y = [ p/float(self.total_agents) for p in self.population_data[s] ]
+            #args.extend([data_x, data_y, self.style(i_s)])
+            plt.plot( data_x, data_y, self.style(i_s), label=s.name )
+
+        plt.legend(ncol = 3)
+
         plt.xlim(0, self.n)
         plt.ylim(0, 1)
         plt.xlabel('Generations')
         plt.ylabel('populations')
         plt.grid(True)
-
-        plt.show()
+        plt.savefig( self.filename )
--- python/IPD/run.py
+++ python/IPD/run.py
@@ -2,7 +2,11 @@
 
 # Standard
 import random
+import os
+import signal
+import sys
 
+# Arenas
 from arena.AllPairings import AllPairings
 from arena.Torus import Torus
 from arena.Rectangle import Rectangle
@@ -19,15 +23,29 @@ from monitoring.PopulationFractions import PopulationFractions
 import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
 argParser.add_argument('--strategies',  action='store', nargs='*',  choices=[s.name for s in strategies], default=['TFT', 'TFTT', 'Random'], help="Strategies." )
-argParser.add_argument('--size', default=50, action='store', type=int, help = 'size of Arena')
-argParser.add_argument('--nmax', default=100, action='store', type=int, help = 'Maximum number of iterations')
+argParser.add_argument('--arena',  action='store', type=str, default='Torus', choices = ['Torus', 'Rectangle', 'Line', 'Circle', 'AllPairings'], help="Output directory." )
+argParser.add_argument('--size', default=20, action='store', type=int, help = 'size of Arena')
+argParser.add_argument('--nmax', default=1000, action='store', type=int, help = 'Maximum number of iterations')
+argParser.add_argument('--output',  action='store', type=str, default='/Users/robertschoefbeck/Desktop/', help="Output directory." )
 
 #argParser.add_argument('--prob', default=0.05, action='store', type=float )
 
 args = argParser.parse_args()
+if args.arena == 'Torus':
+    arena = Torus( nx = args.size, ny = args.size)
+elif args.arena == 'Rectangle':
+    arena = Rectangle( nx = args.size, ny = args.size)
+elif args.arena == 'Line':
+    arena = Line( n = args.size) 
+elif args.arena == 'Circle':
+    arena = Circle( n = args.size) 
+elif args.arena == 'AllPairings':
+    arena = AllPairings( n = args.size)
+else:
+    raise ValueError( "Don't know about arena %s." % args.arena )
 
-arena = Torus( nx = args.size, ny = args.size)
-arena.add_monitoring_element( PopulationFractions() )
+
+arena.add_monitoring_element( PopulationFractions( filename = os.path.join( args.output, 'PopulationFractions.pdf') ) )
 
 strategies_in_arena = []
 for i_s, s in enumerate(args.strategies):
@@ -42,23 +60,42 @@ counter = 0
 print "Start with"
 print arena
 print
+
+def SIGINT_handler(signal, frame):
+    print('You pressed Ctrl+C! Writing monitor elements, then stop.')
+
+    # Write result
+    arena.write_monitoring_elements()
+    print "Bye."
+    sys.exit( 0 )
+
+# signal.signal(signal.SIGINT, SIGINT_handler)
+#print('Press Ctrl+C')
+#signal.pause()
+
 while True:
+    try:
+        print "At", counter
+        arena.iterate()
+        counter += 1
+        print arena
+        arena.print_population_performance()
 
-    print "At", counter
-    arena.iterate()
-    counter += 1
-    print arena
-    arena.print_population_performance()
+        if len(set(map(type, arena.agents.values())))==1: 
+            print "Converged after %i iterations!" % counter
+            break
 
-    if len(set(map(type, arena.agents.values())))==1: 
-        print "Converged after %i iterations!" % counter
-        break
-
-    if args.nmax>0 and counter>= args.nmax: 
-        print "Stopped after %i iterations!" % counter
-        break
-
+        if args.nmax>0 and counter>= args.nmax: 
+            print "Stopped after %i iterations!" % counter
+            break
+    except KeyboardInterrupt:
+        print "Bye."
+        arena.write_monitoring_elements()
+        sys.exit()
 print
 arena.evaluate_performance()
 print arena
 arena.print_population_performance()
+
+# Write result
+arena.write_monitoring_elements()
